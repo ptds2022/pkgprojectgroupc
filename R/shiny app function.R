0dc949d08acca25944accd8634158b074463afa3
franzwetzel <franz.wetzel@unil.ch>
2022-12-15 19:30:24 +0100

    add documentation shiny function

---
+++ R/shiny app function.R
@@ -0,0 +1,14 @@
+#' @title shiny app
+#'
+#' @description webpage for finding the best gas station of the user.
+#' The shiny app includes widgets for adjusting the distance in which gas station should be considered
+#' and for the type of fuel the user needs. The app issues the name of the most suitable gas station
+#' and a map with the location of the user and all the gas stations which are in the given distance.
+#' @return A shiny app which can be used to find the best gas station
+#' @author Clarence, Franz, Mathieu, Lukas
+#' @export
+#' @examples
+#' "hallo"
+shiny_gas_stations <- function(){
+
+}
